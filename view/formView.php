bf4b76cc9e94e33fbc3a8aaac2f3436c9b1d9ca1
shivamtg <shivamtg1376@gmail.com>
2022-07-01 11:17:19 +0200

    add buttons to switch between 2 pages

---
+++ view/formView.php
@@ -0,0 +1,10 @@
+<?php ?>
+<div class="row align-items-center">
+    <div class="col-12 text-center">
+        <form method="get">
+            <button class="btn btn-outline-success" name="page" value="homePage">Home Page</button>
+            <!--<button class="btn btn-outline-info" name="page" value="form">Add a message</button>-->
+        </form>
+        <p>user input form goes here</p>
+    </div>
+</div>
--- view/homePageView.php
+++ view/homePageView.php
@@ -2,9 +2,11 @@
 
 <div class="row align-items-center">
     <div class="col-12 text-center">
-        <form action="../index.php" method="get">
-            <button class="btn btn-outline-success" name="page" value="homePage">Home Page</button>
+        <form method="get">
+            <!--<button class="btn btn-outline-success" name="page" value="homePage">Home Page</button>-->
             <button class="btn btn-outline-info" name="page" value="form">Add a message</button>
         </form>
+        <p>All the post will be shown here</p>
     </div>
 </div>
+
